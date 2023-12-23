6107b36be23db1ba9381a2315fd0c1969bb1a8e0
DELL <yatishkumarbs445@gmail.com>
2023-12-23 21:21:13 +0530

    1 line added

--- demo.py
+++ demo.py
@@ -1,1 +1,19 @@
-print("Hello)
+print("Hello")
+a=10
+b=20
+class cal():
+    def add(a,b):
+        print(a+b)
+
+    def sub(a,b):
+        print(a-b)
+
+    def mul(a,b):
+        print(a*b)
+
+
+
+m=cal()
+cal.add(8,5)
+cal.sub(9,3)
+cal.mul(8,5)
